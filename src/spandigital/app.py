0c389951ee3bc2a7eff44ff70de57a9d126d4c3a
Eddie Jobs <iamezcua.dev@gmail.com>
2026-04-21 00:20:51 -0600

    Add capability to read input from files
    - Add a single test case.

--- src/spandigital/app.py
+++ src/spandigital/app.py
@@ -1,5 +1,22 @@
+import sys
+
+
+def read_input():
+    raw_data = []
+
+    if '--filename' in sys.argv:
+        filename = sys.argv[sys.argv.index('--filename') + 1]
+        with open(filename, 'r') as f:
+            raw_data = f.read().splitlines()
+
+    if len(raw_data) > 0:
+        raw_data = [data for data in raw_data if data.strip() != ""]
+
+    return raw_data
+
+
 def main() -> None:
-    print("Hello World!")
+    raw_data: list[str] = read_input()
 
 
 if __name__ == "__main__":
