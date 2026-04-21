7b3ebd084b13e32c642f7c4cd3c000f0dcc4e4b5
Eddie Jobs <iamezcua.dev@gmail.com>
2026-04-21 00:37:15 -0600

    Create program flow with unimplemented functions
    - This is useful to defing contracts even before implementations.

--- src/spandigital/app.py
+++ src/spandigital/app.py
@@ -15,8 +15,19 @@ def read_input():
     return raw_data
 
 
+def compute_scoreboard(data: list[str]) -> dict[str, int]:
+    pass
+
+
+def get_teams_ranking(scoreboard: dict[str, int]) -> list[list[str]]:
+    pass
+
+
 def main() -> None:
     raw_data: list[str] = read_input()
+    scoreboard: dict[str, int] = compute_scoreboard(raw_data)
+    ranking: list[list[str]] = get_teams_ranking(scoreboard)
+    print(ranking)
 
 
 if __name__ == "__main__":
