7952c94c7031445611de900b1db75e755b5995d6
Eddie Jobs <iamezcua.dev@gmail.com>
2026-04-21 02:08:51 -0600

    Implement scoreboard function

--- src/spandigital/app.py
+++ src/spandigital/app.py
@@ -15,8 +15,41 @@ def read_input():
     return raw_data
 
 
-def compute_scoreboard(data: list[str]) -> dict[str, int]:
-    pass
+def compute_scoreboard(game_matches: list[str]) -> dict[str, int]:
+    scoreboard = {}
+
+    scores = {
+        'win': 3,
+        'tie': 1
+    }
+
+    for game_match in game_matches:
+        if not ',' in game_match:
+            print(f"Skipping malformed line: {game_match}...")
+        else:
+            team_and_score1, team_and_score2 = game_match.rsplit(',', 1)
+            if team_and_score1 == '' or team_and_score2 == '':
+                print(f"Skipping malformed line: {game_match}...")
+            else:
+                team_score1 = team_and_score1.strip().rsplit(' ', 1)
+                team_score2 = team_and_score2.strip().rsplit(' ', 1)
+                team1 = team_score1[0]
+                team2 = team_score2[0]
+                import re
+                score1 = int(re.sub(r'\D', '', team_score1[1]))
+                score2 = int(re.sub(r'\D', '', team_score2[1]))
+
+                if score1 > score2:
+                    scoreboard[team1] = scoreboard.get(team1, 0) + scores['win']
+                    scoreboard[team2] = scoreboard.get(team2, 0)
+                elif score1 == score2:
+                    scoreboard[team1] = scoreboard.get(team1, 0) + scores['tie']
+                    scoreboard[team2] = scoreboard.get(team2, 0) + scores['tie']
+                else:
+                    scoreboard[team1] = scoreboard.get(team1, 0)
+                    scoreboard[team2] = scoreboard.get(team2, 0) + scores['win']
+
+    return scoreboard
 
 
 def get_teams_ranking(scoreboard: dict[str, int]) -> list[list[str]]:
